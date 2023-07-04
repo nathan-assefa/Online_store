4d65f2cc708c676a5b84ee66c598c3440e26e8a1
mahlet <akotet2123@gmail.com>
2023-07-04 03:12:10 +0000

    landing page added

--- web_flask/online_store.py
+++ web_flask/online_store.py
@@ -18,19 +18,7 @@ def close_db(exit):
 # Landing Page
 @app.route('/', strict_slashes=False)
 def landing_page():
-    return render_template('landing_page.html')
-
-@app.route("/products/<product_id>", strict_slashes=False)
-def product(product_id):
-    product = storage.get(Product, product_id)
-    all_products = storage.all(Product).values()
-    category_name = ""
-    related_products = []
-
-    for pro in all_products:
-        if pro.category.name == product.category.name:
-            for url in pro.urls[:1]:
-                related_products.append(url.link)
+    """
     if product.category.name == 'dressings':
         category_name = "Glamourra"
     elif product.category.name == 'shoe':
@@ -39,16 +27,8 @@ def product(product_id):
         category_name = "Glimmera"
     elif product.category.name == 'tech':
         category_name = "Technomancy"
-
-    images = [url.link for url in product.urls]
-
-    return render_template(
-            'sub_page.html',
-            images=images,
-            product=product,
-            category_name=category_name,
-            related_products=related_products
-            )
+    """
+    return render_template('landing_page.html')
 
 # Home Page
 @app.route('/shop', strict_slashes=False)
