8c98a626aedee45bc20ad32cc4e14a3995ed8fca
mahlet <akotet2123@gmail.com>
2023-07-12 22:34:56 +0000

    authentication blueprint

--- web_flask/version01/app.py
+++ web_flask/version01/app.py
@@ -2,7 +2,6 @@
 """ Flask Application """
 from os import getenv
 from models import storage
-from web_flask.version01.views import app_views
 from flask import Flask, render_template, make_response, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -10,56 +9,48 @@ from flask_login import LoginManager
 from flask_cors import CORS
 
 
-"""
-db = SQLAlchemy()
 bcrypt = Bcrypt()
-"""
-
-app = Flask(__name__)
-
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+mysqldb://{}:{}@{}/{}'.format(
-        getenv('ONLINE_STORE_MYSQL_USER'),
-        getenv('ONLINE_STORE_MYSQL_PWD'),
-        getenv('ONLINE_STORE_MYSQL_HOST'),
-        getenv('ONLINE_STORE_MYSQL_DB')
-        )
-
-app.config["SECRET_KEY"] = "thisidsupposedtobeasecretkey"
-
-
-"""
 db = SQLAlchemy()
-bcrypt = Bcrypt()
-"""
-"""
-db.init_app(app)
-bcrypt.init_app(app)
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = "login"
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    user = storage.user_by_id(user_id)
-    return user
-"""
-app.register_blueprint(app_views)
+def create_app():
+    app = Flask(__name__)
 
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+    
+    app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.db'
+    """
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+mysqldb://{}:{}@{}/{}'.format(
+            getenv('ONLINE_STORE_MYSQL_USER'),
+            getenv('ONLINE_STORE_MYSQL_PWD'),
+            getenv('ONLINE_STORE_MYSQL_HOST'),
+            getenv('ONLINE_STORE_MYSQL_DB')
+            )
+    """
 
-@app.teardown_appcontext
-def close_db(exit):
-    """This context function gives back the
-    connection once request is done"""
-    storage.close()
+    app.config["SECRET_KEY"] = "thisidsupposedtobeasecretkey"
 
-@app.errorhandler(404)
-def not_found(error):
-    #return render_template("404.html")
-    return make_response(jsonify({'error': "Not found"}), 404)
+    bcrypt.init_app(app)
+    db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = "app_auth.login"
+
+    # blueprint for authentication routes
+    from web_flask.version01.views.authentication_app import app_auth
+    app.register_blueprint(app_auth, db=db)
+
+    cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        user = storage.user_by_id(user_id)
+        return user
+
+    return app
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(host="0.0.0.0", port=5000, debug=True)
--- web_flask/version01/views/authentication_app.py
+++ web_flask/version01/views/authentication_app.py
@@ -1,40 +1,20 @@
 #/usr/bin/python3
 """ app module """
-import sys
-from flask import Flask, render_template, request, redirect, url_for, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, url_for, jsonify, make_response, current_app
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
-from flask_bcrypt import Bcrypt
-from os import getenv
 from web_flask.version01.forms.forms import RegisterForm
 from web_flask.version01.forms.forms import LoginForm
-from models.user import User
+#from models.user import User
+from web_flask.version01.models import User
 from models import storage
-from web_flask.version01.views import app_views
-#from web_flask.version01.app import bcrypt, db
+from web_flask.version01.app import db, bcrypt
+from flask import Blueprint
 
 
-
-@app_views.route("/login", methods=["GET", "POST"])
-def login():
-    form = LoginForm()
-
-    if form.validate_on_submit():
-        user = storage.user_by_email(form.email.data)
-        if user:
-            if bcrypt.check_password_hash(user.password, form.password.data):
-                login_user(user)
-                return redirect(url_for("dashboard", form=form))
-    return render_template("login.html", form=form)
-
-@app_views.route("/logout", methods=["GET", "POST"])
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for("login"))
+app_auth = Blueprint('app_auth', __name__, url_prefix='/gebeya_hub/version01')
 
 
-@app_views.route("/register", methods=["GET", "POST"])
+@app_auth.route("/register", methods=["GET", "POST"])
 def register():
     form = RegisterForm()
 
@@ -45,20 +25,35 @@ def register():
         new_user.last_name=form.lastname.data
         new_user.email=form.email.data
         new_user.password=hashed_password
-
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for("login"))
+        with current_app.app_context():
+            db.session.add(new_user)
+            db.session.commit()
+        return redirect(url_for("app_auth.login"))
 
     return render_template("register.html", form=form)
 
-@app_views.route("/dashboard", methods=["GET", "POST"])
+@app_auth.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = storage.user_by_email(form.email.data)
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for("app_auth.dashboard", form=form))
+    return render_template("login.html", form=form)
+
+@app_auth.route("/logout", methods=["GET", "POST"])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("app_auth.login"))
+
+
+@app_auth.route("/dashboard", methods=["GET", "POST"])
 @login_required
 def dashboard():
     form = LoginForm()
 
     return render_template("dashboard.html", form=form)
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000', debug=True)
